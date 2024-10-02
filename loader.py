c78289f145dca514aa7358e436f830315bfa98b3
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-10-02 09:30:41 -0700

    udpate

--- loader.py
+++ loader.py
@@ -3,6 +3,6 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from data import config
 
-bot = Bot(config.BOT_TOKEN, parse_mode="HTML")
+bot = Bot(config.BOT_TOKEN, parse_mode=ParseMode.HTML)
 stroge = MemoryStorage()
 dp = Dispatcher(storage=stroge)
