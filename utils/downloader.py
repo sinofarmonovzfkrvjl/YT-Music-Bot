f5efed7d0eea47e47de02330e02f0a833b5d9a0f
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-10-02 09:38:58 -0700

    update

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,12 +1,5 @@
 from youtubesearchpython import VideosSearch
 import yt_dlp
-from moviepy.editor import VideoFileClip
-import os
-import glob
-
-def separate_audio_from_video(video_path, output_path="musiqa.mp3"):
-    clip = VideoFileClip(video_path)
-    clip.audio.write_audiofile(output_path)
 
 def Mp3Downloader(url: list) -> list:
     ydl_opts = {
@@ -23,7 +16,3 @@ def Mp3Downloader(url: list) -> list:
             ydl.download(url)
     except Exception as e:
         print(e)
-    # try:
-    #     separate_audio_from_video(glob.glob("*.mp4")[0])
-    # except Exception as e:
-    #     print(e)
